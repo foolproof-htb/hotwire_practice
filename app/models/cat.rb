e8cf810cf1b91cc5ae4c3b169295c22ca6521094
foolproof-htb <fujisawa.htb@gmail.com>
2024-11-09 11:16:38 +0900

    bootstrap_form を導入してバリデーション実装

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,7 @@
 class Cat < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
   def self.ransackable_attributes(_auth_object = nil)
     %w[id name age created_at updated_at]
   end
